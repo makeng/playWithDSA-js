c2b6f30fe7991d896b0228953ad2fdd40949786b
makeng <810768333@qq.com>
2021-05-31 11:03:22 +0800

    🌟: update bubble sort

--- basic/sort-simple-bubble.js
+++ basic/sort-simple-bubble.js
@@ -1,26 +1,33 @@
 const a = [9, 1, 5, 8, 3, 7, 4, 6, 2]
-const b = [2, 1, 3, 4, 5, 6, 7, 8, 9]
+const b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-function swap (list, i, j) {
+function swap(list, i, j) {
   const temp = list[i]
   list[i] = list[j]
   list[j] = temp
 }
 
 // 冒泡
-function bubbleSort (list) {
-  let isNotSwap = false
+function bubbleSort(list) {
+  let cnt = 0
+  let isSwapped = false
 
-  // 如果都没有交换，表示从此往后的都符合了规律，不需要再进行
-  for (let i = 0; i < list.length - 1 && !isNotSwap; i++) {
-    for (let j = 0; j < list.length - i; j++) {
+  for (let i = list.length - 1; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
       if (list[j] > list[j + 1]) {
         swap(list, j, j + 1)
-        isNotSwap = false // 有交换，就表示还需要比较
+        isSwapped = true
       }
+      cnt += 1
+    }
+    // 如果都没有交换，表示从此往后的都符合了规律，不需要再进行
+    if (!isSwapped) {
+      console.log('break')
+      break
     }
   }
 
+  console.log('steps: ', cnt)
   return list
 }
 
